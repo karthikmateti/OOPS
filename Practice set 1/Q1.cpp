029e921713e0436153759ffdb4920a43f4a9bacf
Mateti Karthik <karthikmateti2007@gmail.com>
2025-09-04 10:29:46 +0530

    Add files via upload

---
+++ Practice set 1/Q1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    int age;
+    cout<<"enter name:";
+    cin>>name;
+    cout<<"enter age:";
+    cin>>age;
+    cout<<"Hello "<< name <<" you are "<< age <<" years old."<<endl;
+    return 0;
+}
