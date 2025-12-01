2b138f3f85e7d4bbe3dee4acd5961ee2a8a0268a
Mateti Karthik <karthikmateti2007@gmail.com>
2025-12-01 09:55:27 +0530

    Update Q7.cpp

--- Mateti karthik_Ass 3/Q7.cpp
+++ Mateti karthik_Ass 3/Q7.cpp
@@ -5,7 +5,7 @@ using namespace std;
 bool valid(string s){
     stack<char>st;
     for(char c:s){
-        if(c=='('||c=='}'||c=='[') st.push(c);
+        if(c=='('||c=='{'||c=='[') st.push(c);
         else{
             if(st.empty())return false;
             char t=st.top();
